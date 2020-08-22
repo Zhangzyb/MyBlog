11a84bc7fad6e01e323f9e2d6442fa93697167a0
zyb <841733577@qq.com>
2020-08-22 20:09:29 +0800

    设定后台展示的字段

--- Blog/article/admin.py
+++ Blog/article/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from article.models import Article, Tag, Category
 
-admin.site.register(Article)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'create_time', 'modified_time', 'category', 'author']
+    fields = ['title', 'text', 'abstract', 'category', 'tag']
+
+    def save_model(self, request, obj, form, change):
+        obj.author = request.user
+        super().save_model(request, obj, form, change)
+
+
+admin.site.register(Article, PostAdmin)
 admin.site.register(Tag)
 admin.site.register(Category)
+
 
