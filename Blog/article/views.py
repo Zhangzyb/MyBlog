8730c8c10db6b6d5a7b1d88fd505aedbc8493b97
zyb <841733577@qq.com>
2020-08-23 23:28:22 +0800

    完成博客详情页函数

--- Blog/article/views.py
+++ Blog/article/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from article.models import Article
+
+
+def index(request):
+    post_list = Article.objects.all().order_by('-create_time')
+    return render(request, 'index.html', context={'post_list': post_list})
+
+
+def detail(request,pk):
+    article = get_object_or_404(Article, pk=pk)
+    return render(request, 'detail.html', context={'article':article})
