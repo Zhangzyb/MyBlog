d6747e6057f2c2515540e89b18bc2baea998bda2
zyb <841733577@qq.com>
2020-08-24 22:23:38 +0800

    完成前端对Markdown语法的显示

--- Blog/article/views.py
+++ Blog/article/views.py
@@ -1,5 +1,5 @@
+import markdown
 from django.shortcuts import render, get_object_or_404
-
 from article.models import Article
 
 
@@ -8,6 +8,11 @@ def index(request):
     return render(request, 'index.html', context={'post_list': post_list})
 
 
-def detail(request,pk):
+def detail(request, pk):
     article = get_object_or_404(Article, pk=pk)
+    article.text = markdown.markdown(article.text,
+                                     extensions=[
+                                         'markdown.extensions.extra',
+                                         'markdown.extensions.codehilite',
+                                     ])
     return render(request, 'detail.html', context={'article':article})
