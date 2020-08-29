38d714e418ae753883b91ea3604e12d099a14f2d
zyb <841733577@qq.com>
2020-08-29 20:41:11 +0800

    增加按归档、标签查询视图

--- Blog/article/views.py
+++ Blog/article/views.py
@@ -1,6 +1,6 @@
 import markdown
 from django.shortcuts import render, get_object_or_404
-from article.models import Article
+from article.models import Article, Tag
 
 
 def index(request):
@@ -8,11 +8,22 @@ def index(request):
     return render(request, 'index.html', context={'post_list': post_list})
 
 
-def detail(request, pk):
-    article = get_object_or_404(Article, pk=pk)
+def detail(request, english_name):
+    article = get_object_or_404(Article, english_name=english_name)
     article.text = markdown.markdown(article.text,
                                      extensions=[
                                          'markdown.extensions.extra',
                                          'markdown.extensions.codehilite',
                                      ])
-    return render(request, 'detail.html', context={'article':article})
+    return render(request, 'detail.html', context={'article': article})
+
+
+def archive(request, year, month):
+    article_list = Article.objects.filter(create_time__year=year, create_time__month=month).order_by('-create_time')
+    date = {'year': year, 'month': month}
+    return render(request, 'archive_article.html', context={'article_list': article_list, 'date': date})
+
+
+def tag(request, name):
+    article_list = Tag.objects.filter(name=name)
+    return render(request, 'tag_article.html', context={'article_list':  article_list, 'name': name})
