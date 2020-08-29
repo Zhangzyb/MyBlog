b3094257df4732415561ffb293aadd20d15f1a7a
zyb <841733577@qq.com>
2020-08-29 20:33:00 +0800

    增加文章标签

--- Blog/article/templatetags/article_extra.py
+++ Blog/article/templatetags/article_extra.py
@@ -4,13 +4,19 @@ from article.models import Article, Tag
 register = template.Library()
 
 
-@register.inclusion_tag('archive.html')
+@register.inclusion_tag('_article.html')
+def articles():
+    article_list = Article.objects.all()
+    return {'article_list': article_list}
+
+
+@register.inclusion_tag('_archive.html')
 def archive():
     date_list = Article.objects.dates('create_time', 'month')
     return {'date_list': date_list}
 
 
-@register.inclusion_tag('tags.html')
+@register.inclusion_tag('_tags.html')
 def tag():
     tag_list = Tag.objects.all()
     return {'tag_list': tag_list}
