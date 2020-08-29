6a38e93237ffdf59fb37d4db09ccedde28f2040c
zyb <841733577@qq.com>
2020-08-29 20:42:08 +0800

    增加归档、标签查询路由

--- Blog/article/url.py
+++ Blog/article/url.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'article'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('articles/<int:pk>/', views.detail, name='detail'),
+    path('articles/<slug:english_name>/', views.detail, name='detail'),
+    path('archives/<int:year>/<int:month>/', views.archive, name='archive'),
+    path('tags/<slug:name>/', views.tag, name='tag'),
 ]
