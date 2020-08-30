a0ce477b16568abd678056830619a741390062d6
zyb <841733577@qq.com>
2020-08-30 14:10:01 +0800

    分类模型重定义：增加多级目录

--- Blog/article/models.py
+++ Blog/article/models.py
@@ -1,11 +1,14 @@
+import markdown
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
+from django.utils.html import strip_tags
 from mdeditor.fields import MDTextField
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField('分类', max_length=50)
+    parent = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, related_name='subdir', verbose_name='上级目录')
 
     class Meta:
         verbose_name = '分类'
@@ -28,11 +31,13 @@ class Tag(models.Model):
 
 class Article(models.Model):
     title = models.CharField('标题',max_length=200)
+    english_name = models.CharField('英文名', max_length=100)
     text = MDTextField('正文')
+    image = models.ImageField(upload_to='img/%Y/%m/%d')
     create_time = models.DateTimeField('创建时间', default=timezone.now)
     modified_time = models.DateTimeField('修改时间')
     author = models.ForeignKey(User, verbose_name='作者', on_delete=models.CASCADE)
-    abstract = models.CharField('摘要', max_length=500)
+    abstract = models.CharField('摘要', max_length=1000)
     tag = models.ManyToManyField(Tag, verbose_name='标签')
     category = models.ForeignKey(Category, verbose_name='分类', on_delete=models.CASCADE)
 
@@ -45,4 +50,9 @@ class Article(models.Model):
 
     def save(self, *args, **kwargs):
         self.modified_time = timezone.now()
+        md = markdown.Markdown(extensions=[
+            'markdown.extensions.extra',
+            'markdown.extensions.codehilite',
+        ])
+        self.abstract = strip_tags(md.convert(self.text))[:260]
         super().save(*args, **kwargs)
