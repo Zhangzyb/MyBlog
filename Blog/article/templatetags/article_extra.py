89eeb0c01c9aab0b4a2b25d57ac85c833892d1ee
zyb <841733577@qq.com>
2020-08-31 08:40:27 +0800

    删除文章标签，增加自一级、二级菜单标签

--- Blog/article/templatetags/article_extra.py
+++ Blog/article/templatetags/article_extra.py
@@ -4,25 +4,33 @@ from article.models import Article, Tag, Category
 register = template.Library()
 
 
-@register.inclusion_tag('_article.html')
-def articles():
-    article_list = Article.objects.all()
-    return {'article_list': article_list}
-
-
 @register.inclusion_tag('_archive.html')
 def archive():
     date_list = Article.objects.dates('create_time', 'month')
-    return {'date_list': date_list}
+    return {
+        'date_list': date_list
+    }
 
 
 @register.inclusion_tag('_tags.html')
 def tag():
     tag_list = Tag.objects.all()
-    return {'tag_list': tag_list}
+    return {
+        'tag_list': tag_list
+    }
 
 
-@register.inclusion_tag('_category.html', takes_context=True)
-def category(context):
-    subdir_list = Category.objects.filter(parent_id=context['category'].id)
-    return {'subdir_list': subdir_list}
+@register.inclusion_tag('_category.html')
+def category():
+    category_list = Category.objects.filter(parent__isnull=True)
+    return {
+        'category_list': category_list
+    }
+
+
+@register.inclusion_tag('_subcategory.html', takes_context=True)
+def subcategory(context):
+    subcategory_list = Category.objects.filter(parent_id=context['category'].id)
+    return {
+        'subcategory_list': subcategory_list
+    }
