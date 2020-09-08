7c0f6b7ae6b47f0e66e3231a05b99c55f19a0857
zyb <841733577@qq.com>
2020-09-08 11:02:55 +0800

    利用Django模型创建评论表单

---
+++ Blog/comment/forms.py
@@ -0,0 +1,23 @@
+from django.forms import ModelForm, TextInput, EmailInput, Textarea
+from .models import Comment
+
+
+class CommentForm(ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['name', 'email', 'text']
+        widgets = {
+            'name': TextInput(attrs={
+                'placeholder': '昵称',
+                'class': 'form-control form-control-lg',
+            }),
+            'email': EmailInput(attrs={
+                'placeholder': '邮箱',
+                'class': 'form-control form-control-lg',
+            }),
+            'text': Textarea(attrs={
+                'class': 'form-control', 'rows': 3,
+                'id': 'exampleFormControlTextarea1',
+                'placeholder': '说点什么吧^_^'
+            }),
+        }
