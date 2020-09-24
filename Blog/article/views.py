3024cab102aae248a4675635b218300ad9a0500a
zyb <841733577@qq.com>
2020-09-24 15:42:22 +0800

    增加分页功能

--- Blog/article/views.py
+++ Blog/article/views.py
@@ -1,11 +1,16 @@
 import markdown
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from .models import Article, Tag, Category
 
 
 def index(request):
-    article_list = Article.objects.all()
-    return render(request, 'index.html', context={'article_list': article_list})
+    articles = Article.objects.all()
+    paginator = Paginator(articles, 5)
+    page_number = request.GET.get('page')
+    article_list = paginator.get_page(page_number)
+    page_range = pagination(article_list)
+    return render(request, 'index.html', context={'article_list': article_list, 'page_range': page_range})
 
 
 def detail(request, english_name):
@@ -29,26 +34,46 @@ def detail(request, english_name):
 
 
 def archive(request, year, month):
-    article_list = Article.objects.filter(create_time__year=year, create_time__month=month).order_by('-create_time')
-    info = {
-        'info': f'{year}/{month}'
-    }
-    return render(request, 'index.html', context={'article_list': article_list, 'info': info})
+    articles = Article.objects.filter(create_time__year=year, create_time__month=month).order_by('-create_time')
+    paginator = Paginator(articles, 5)
+    page_number = request.GET.get('page')
+    article_list = paginator.get_page(page_number)
+    page_range = pagination(article_list)
+    info = {'info': f'{year}/{month}'}
+    return render(request, 'index.html', context={'article_list': article_list, 'info': info, 'page_range': page_range})
 
 
 def tag(request, name):
     tag_id = Tag.objects.filter(name=name).first()
-    article_list = Article.objects.filter(tag=tag_id)
-    info = {
-        'info': name
-    }
-    return render(request, 'index.html', context={'article_list':  article_list, 'info': info})
+    articles = Article.objects.filter(tag=tag_id)
+    paginator = Paginator(articles, 5)
+    page_number = request.GET.get('page')
+    article_list = paginator.get_page(page_number)
+    page_range = pagination(article_list)
+    info = {'info': name}
+    return render(request, 'index.html', context={'article_list':  article_list, 'info': info, 'page_range': page_range})
 
 
 def category(request, cate_name):
     category_id = Category.objects.filter(cate_name=cate_name).first()
-    article_list = Article.objects.filter(category=category_id)
-    info = {
-        'info': cate_name
-    }
-    return render(request, 'index.html', context={'article_list': article_list, 'info': info})
+    articles = Article.objects.filter(category=category_id)
+    paginator = Paginator(articles, 5)
+    page_number = request.GET.get('page')
+    article_list = paginator.get_page(page_number)
+    page_range = pagination(article_list)
+    info = {'info': cate_name}
+    return render(request, 'index.html', context={'article_list': article_list, 'info': info, 'page_range': page_range})
+
+
+def pagination(article_list):
+    page = article_list.number
+    page_range = list(range(max(page - 2, 1), min(page + 3, article_list.paginator.num_pages + 1)))
+    if page_range[0] - 1 >= 2:
+        page_range.insert(0, '...')
+    if article_list.paginator.num_pages - page_range[-1] >= 2:
+        page_range.append('...')
+    if page_range[0] != 1:
+        page_range.insert(0, 1)
+    if page_range[-1] != article_list.paginator.num_pages:
+        page_range.append(article_list.paginator.num_pages)
+    return page_range
