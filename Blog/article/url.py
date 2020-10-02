279d771514138be9fce3d0241d18d39ba25aa03c
zyb <841733577@qq.com>
2020-10-02 22:51:54 +0800

    配置点赞路由

--- Blog/article/url.py
+++ Blog/article/url.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('articles/<slug:english_name>/', views.detail, name='detail'),
     path('archives/<int:year>/<int:month>/', views.archive, name='archive'),
     path('tags/<slug:name>/', views.tag, name='tag'),
-    path('category/<slug:cate_name>', views.category, name='category'),
+    path('category/<slug:cate_name>/', views.category, name='category'),
+    path('article/inc_like/<slug:english_name>/', views.add_likes, name='likes'),
 ]
