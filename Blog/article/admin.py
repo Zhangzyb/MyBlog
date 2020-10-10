d208d8e67dc4f4990da47cdeb99f9a3d37a9f297
zyb <841733577@qq.com>
2020-10-10 23:19:33 +0800

    注册点赞表

--- Blog/article/admin.py
+++ Blog/article/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Article, Tag, Category
+from .models import Article, Tag, Category, PostLikes
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -14,5 +14,6 @@ class PostAdmin(admin.ModelAdmin):
 admin.site.register(Article, PostAdmin)
 admin.site.register(Tag)
 admin.site.register(Category)
+admin.site.register(PostLikes)
 
 
