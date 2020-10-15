174659b915d5131868310882e1c9469cd2f9ad81
zyb <841733577@qq.com>
2020-10-15 09:42:23 +0800

    修复删除父分类而子分类未删除的Bug

--- Blog/article/models.py
+++ Blog/article/models.py
@@ -9,7 +9,7 @@ from mdeditor.fields import MDTextField
 class Category(models.Model):
     name = models.CharField('分类', max_length=50)
     cate_name = models.CharField('英文名', max_length=50)
-    parent = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='subdir', verbose_name='上级目录')
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='subdir', verbose_name='上级目录')
 
     class Meta:
         verbose_name = '分类'
